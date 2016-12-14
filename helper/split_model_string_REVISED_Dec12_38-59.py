ef87465cfc5adcd97244e36eeaa81b6dfea78d69
Jhave <jhave2@gmail.com>
2016-12-14 18:46:41 +0800

    One Month Models ALL GENERATED
    Trained neural nets on 11k poems (Poetry Foundation corpus) for over a
    month. Began on Wed Oct 26 15:24:30 , ended Dec 12, 2016 at 12:37 pm
    
    Changed the wavelet parameters (dilation channels, etc) and ran 5
    parallel trainings on the same data in separate tabs in Terminal.
    
    Saved all models with loss under 0.7
    Generated poems for 3 of those parameter settings (the ones that lasted
    the longest before blowing out).
    
    What is a model?
    A model is the neural net’s best guess at that moment about reality.
    Think of each model as a writer.
    Some are very young; others incoherent; some have lots of range; others
    are always very stern and linefeed precisely. The last ones are senile,
    since the neural net systems seem to enter into oscillatory runaway
    values.
    
    My current stash of models:
    579,804,106,829 bytes (579.82 GB on disk) for 6,210 items

---
+++ helper/split_model_string_REVISED_Dec12_38-59.py
@@ -0,0 +1,469 @@
+
+import pandas as pd
+import numpy as np
+
+
+
+string="""
+
+
+
+
+---
+loss_value= 0.63918 
+---
+
+Storing checkpoint to ./logdir/train/2016-10-27T10-38-59 ...checkpoint_path= ./logdir/train/2016-10-27T10-38-59/model.ckpt-111204
+ Done.
+step 111205 - loss = 0.680, (28.549 sec/step)
+
+---
+loss_value= 0.680373 
+---
+
+Storing checkpoint to ./logdir/train/2016-10-27T10-38-59 ...checkpoint_path= ./logdir/train/2016-10-27T10-38-59/model.ckpt-111205
+ Done.
+step 111206 - loss = 0.783, (25.987 sec/step)
+
+step 111424 - loss = 0.691, (28.499 sec/step)
+
+---
+loss_value= 0.690689 
+---
+
+Storing checkpoint to ./logdir/train/2016-10-27T10-38-59 ...checkpoint_path= ./logdir/train/2016-10-27T10-38-59/model.ckpt-111424
+ Done.
+step 111425 - loss = 0.857, (28.467 sec/step)
+
+
+step 111878 - loss = 0.693, (18.998 sec/step)
+
+---
+loss_value= 0.692801 
+---
+
+Storing checkpoint to ./logdir/train/2016-10-27T10-38-59 ...checkpoint_path= ./logdir/train/2016-10-27T10-38-59/model.ckpt-111878
+ Done.
+step 111879 - loss = 1.078, (16.773 sec/step)
+
+
+---
+loss_value= 0.607538 
+---
+
+Storing checkpoint to ./logdir/train/2016-10-27T10-38-59 ...checkpoint_path= ./logdir/train/2016-10-27T10-38-59/model.ckpt-112009
+ Done.
+step 112010 - loss = 0.731, (21.086 sec/step)
+step 112011 - loss = 0.698, (21.112 sec/step)
+
+---
+loss_value= 0.697721 
+---
+
+Storing checkpoint to ./logdir/train/2016-10-27T10-38-59 ...checkpoint_path= ./logdir/train/2016-10-27T10-38-59/model.ckpt-112011
+ Done.
+step 112012 - loss = 0.620, (21.296 sec/step)
+
+---
+loss_value= 0.61979 
+---
+
+Storing checkpoint to ./logdir/train/2016-10-27T10-38-59 ...checkpoint_path= ./logdir/train/2016-10-27T10-38-59/model.ckpt-112012
+ Done.
+step 112013 - loss = 1.214, (21.358 sec/step)
+step 112014 - loss = 1.025, (21.330 sec/step)
+step 112015 - loss = 0.930, (21.086 sec/step)
+step 112016 - loss = 0.968, (21.162 sec/step)
+step 112017 - loss = 0.796, (21.241 sec/step)
+step 112018 - loss = 0.725, (21.076 sec/step)
+step 112019 - loss = 0.527, (21.157 sec/step)
+
+---
+loss_value= 0.52721 
+---
+
+Storing checkpoint to ./logdir/train/2016-10-27T10-38-59 ...checkpoint_path= ./logdir/train/2016-10-27T10-38-59/model.ckpt-112019
+ Done.
+step 112020 - loss = 1.063, (21.086 sec/step)
+
+
+step 115895 - loss = 0.687, (25.194 sec/step)
+
+---
+loss_value= 0.686537 
+---
+
+Storing checkpoint to ./logdir/train/2016-10-27T10-38-59 ...checkpoint_path= ./logdir/train/2016-10-27T10-38-59/model.ckpt-115895
+ Done.
+step 115896 - loss = 0.607, (23.678 sec/step)
+
+---
+loss_value= 0.607039 
+---
+
+Storing checkpoint to ./logdir/train/2016-10-27T10-38-59 ...checkpoint_path= ./logdir/train/2016-10-27T10-38-59/model.ckpt-115896
+ Done.
+step 115897 - loss = 1.046, (25.202 sec/step)
+
+---
+loss_value= 0.638347 
+---
+
+Storing checkpoint to ./logdir/train/2016-10-27T10-38-59 ...checkpoint_path= ./logdir/train/2016-10-27T10-38-59/model.ckpt-116309
+ Done.
+step 116310 - loss = 0.636, (21.220 sec/step)
+
+---
+loss_value= 0.636296 
+---
+
+Storing checkpoint to ./logdir/train/2016-10-27T10-38-59 ...checkpoint_path= ./logdir/train/2016-10-27T10-38-59/model.ckpt-116310
+ Done.
+step 116311 - loss = 0.517, (21.240 sec/step)
+
+---
+loss_value= 0.51679 
+---
+
+Storing checkpoint to ./logdir/train/2016-10-27T10-38-59 ...checkpoint_path= ./logdir/train/2016-10-27T10-38-59/model.ckpt-116311
+ Done.
+step 116312 - loss = 1.394, (20.972 sec/step)
+
+---
+loss_value= 0.55477 
+---
+
+Storing checkpoint to ./logdir/train/2016-10-27T10-38-59 ...checkpoint_path= ./logdir/train/2016-10-27T10-38-59/model.ckpt-117766
+ Done.
+step 117767 - loss = 1.229, (25.994 sec/step)
+
+
+step 117848 - loss = 0.616, (28.514 sec/step)
+
+---
+loss_value= 0.615783 
+---
+
+Storing checkpoint to ./logdir/train/2016-10-27T10-38-59 ...checkpoint_path= ./logdir/train/2016-10-27T10-38-59/model.ckpt-117848
+ Done.
+step 117849 - loss = 1.320, (27.925 sec/step)
+step 117850 - loss = 1.388, (28.969 sec/step)
+step 117851 - loss = 1.198, (26.983 sec/step)
+step 117852 - loss = 1.152, (30.412 sec/step)
+step 117853 - loss = 1.049, (26.805 sec/step)
+step 117854 - loss = 1.009, (30.208 sec/step)
+step 117855 - loss = 0.899, (26.446 sec/step)
+step 117856 - loss = 1.027, (30.616 sec/step)
+step 117857 - loss = 0.958, (26.361 sec/step)
+step 117858 - loss = 0.912, (30.912 sec/step)
+step 117859 - loss = 0.959, (26.254 sec/step)
+step 117860 - loss = 0.932, (31.358 sec/step)
+step 117861 - loss = 0.841, (26.786 sec/step)
+step 117862 - loss = 0.966, (31.079 sec/step)
+step 117863 - loss = 0.763, (27.080 sec/step)
+step 117864 - loss = 0.811, (28.586 sec/step)
+step 117865 - loss = 0.821, (30.175 sec/step)
+step 117866 - loss = 0.766, (30.706 sec/step)
+step 117867 - loss = 0.820, (27.527 sec/step)
+step 117868 - loss = 0.683, (28.880 sec/step)
+
+---
+loss_value= 0.682583 
+---
+
+Storing checkpoint to ./logdir/train/2016-10-27T10-38-59 ...checkpoint_path= ./logdir/train/2016-10-27T10-38-59/model.ckpt-117868
+ Done.
+step 117869 - loss = 0.778, (27.535 sec/step)
+
+step 118032 - loss = 0.662, (31.831 sec/step)
+
+---
+loss_value= 0.662325 
+---
+
+Storing checkpoint to ./logdir/train/2016-10-27T10-38-59 ...checkpoint_path= ./logdir/train/2016-10-27T10-38-59/model.ckpt-118032
+ Done.
+step 118033 - loss = 1.269, (24.378 sec/step)
+
+
+step 118217 - loss = 0.504, (25.471 sec/step)
+
+---
+loss_value= 0.503933 
+---
+
+Storing checkpoint to ./logdir/train/2016-10-27T10-38-59 ...checkpoint_path= ./logdir/train/2016-10-27T10-38-59/model.ckpt-118217
+ Done.
+step 118218 - loss = 0.560, (29.955 sec/step)
+
+---
+loss_value= 0.559826 
+---
+
+Storing checkpoint to ./logdir/train/2016-10-27T10-38-59 ...checkpoint_path= ./logdir/train/2016-10-27T10-38-59/model.ckpt-118218
+ Done.
+step 118219 - loss = 1.248, (26.794 sec/step)
+step 118220 - loss = 1.300, (29.842 sec/step)
+step 118221 - loss = 1.278, (26.633 sec/step)
+step 118222 - loss = 1.285, (30.296 sec/step)
+step 118223 - loss = 1.283, (25.689 sec/step)
+step 118224 - loss = 1.052, (30.885 sec/step)
+step 118225 - loss = 0.678, (26.028 sec/step)
+
+---
+loss_value= 0.678425 
+---
+
+Storing checkpoint to ./logdir/train/2016-10-27T10-38-59 ...checkpoint_path= ./logdir/train/2016-10-27T10-38-59/model.ckpt-118225
+ Done.
+step 118226 - loss = 0.586, (30.481 sec/step)
+
+---
+loss_value= 0.586357 
+---
+
+Storing checkpoint to ./logdir/train/2016-10-27T10-38-59 ...checkpoint_path= ./logdir/train/2016-10-27T10-38-59/model.ckpt-118226
+ Done.
+step 118227 - loss = 0.660, (26.394 sec/step)
+
+---
+loss_value= 0.659941 
+---
+
+Storing checkpoint to ./logdir/train/2016-10-27T10-38-59 ...checkpoint_path= ./logdir/train/2016-10-27T10-38-59/model.ckpt-118227
+ Done.
+step 118228 - loss = 1.180, (28.666 sec/step)
+
+
+---
+loss_value= 0.626737 
+---
+
+Storing checkpoint to ./logdir/train/2016-10-27T10-38-59 ...checkpoint_path= ./logdir/train/2016-10-27T10-38-59/model.ckpt-118306
+ Done.
+step 118307 - loss = 1.163, (26.406 sec/step)
+
+step 119109 - loss = 0.673, (23.758 sec/step)
+
+---
+loss_value= 0.672846 
+---
+
+Storing checkpoint to ./logdir/train/2016-10-27T10-38-59 ...checkpoint_path= ./logdir/train/2016-10-27T10-38-59/model.ckpt-119109
+ Done.
+step 119110 - loss = 0.663, (27.666 sec/step)
+
+---
+loss_value= 0.662517 
+---
+
+Storing checkpoint to ./logdir/train/2016-10-27T10-38-59 ...checkpoint_path= ./logdir/train/2016-10-27T10-38-59/model.ckpt-119110
+ Done.
+step 119111 - loss = 0.671, (24.442 sec/step)
+
+---
+loss_value= 0.671165 
+---
+
+Storing checkpoint to ./logdir/train/2016-10-27T10-38-59 ...checkpoint_path= ./logdir/train/2016-10-27T10-38-59/model.ckpt-119111
+ Done.
+step 119112 - loss = 0.904, (23.620 sec/step)
+
+
+
+step 119302 - loss = 0.480, (28.023 sec/step)
+
+---
+loss_value= 0.479751 
+---
+
+Storing checkpoint to ./logdir/train/2016-10-27T10-38-59 ...checkpoint_path= ./logdir/train/2016-10-27T10-38-59/model.ckpt-119302
+ Done.
+step 119303 - loss = 0.340, (24.248 sec/step)
+
+---
+loss_value= 0.339801 
+---
+
+Storing checkpoint to ./logdir/train/2016-10-27T10-38-59 ...checkpoint_path= ./logdir/train/2016-10-27T10-38-59/model.ckpt-119303
+ Done.
+step 119304 - loss = 0.341, (25.228 sec/step)
+
+---
+loss_value= 0.341472 
+---
+
+Storing checkpoint to ./logdir/train/2016-10-27T10-38-59 ...checkpoint_path= ./logdir/train/2016-10-27T10-38-59/model.ckpt-119304
+ Done.
+step 119305 - loss = 0.320, (24.257 sec/step)
+
+---
+loss_value= 0.32004 
+---
+
+Storing checkpoint to ./logdir/train/2016-10-27T10-38-59 ...checkpoint_path= ./logdir/train/2016-10-27T10-38-59/model.ckpt-119305
+ Done.
+step 119306 - loss = 0.346, (24.869 sec/step)
+
+---
+loss_value= 0.34556 
+---
+
+Storing checkpoint to ./logdir/train/2016-10-27T10-38-59 ...checkpoint_path= ./logdir/train/2016-10-27T10-38-59/model.ckpt-119306
+ Done.
+step 119307 - loss = 0.816, (24.809 sec/step)
+
+
+step 119368 - loss = 0.550, (22.878 sec/step)
+
+---
+loss_value= 0.549818 
+---
+
+Storing checkpoint to ./logdir/train/2016-10-27T10-38-59 ...checkpoint_path= ./logdir/train/2016-10-27T10-38-59/model.ckpt-119368
+ Done.
+step 119369 - loss = 0.665, (24.209 sec/step)
+
+---
+loss_value= 0.665115 
+---
+
+Storing checkpoint to ./logdir/train/2016-10-27T10-38-59 ...checkpoint_path= ./logdir/train/2016-10-27T10-38-59/model.ckpt-119369
+ Done.
+step 119370 - loss = 0.614, (24.606 sec/step)
+
+---
+loss_value= 0.613771 
+---
+
+Storing checkpoint to ./logdir/train/2016-10-27T10-38-59 ...checkpoint_path= ./logdir/train/2016-10-27T10-38-59/model.ckpt-119370
+ Done.
+step 119371 - loss = 0.789, (22.716 sec/step)
+step 119372 - loss = 0.666, (24.646 sec/step)
+
+---
+loss_value= 0.666487 
+---
+
+Storing checkpoint to ./logdir/train/2016-10-27T10-38-59 ...checkpoint_path= ./logdir/train/2016-10-27T10-38-59/model.ckpt-119372
+ Done.
+step 119373 - loss = 0.928, (24.190 sec/step)
+
+
+
+
+---
+loss_value= 0.656896 
+---
+
+Storing checkpoint to ./logdir/train/2016-10-27T10-38-59 ...checkpoint_path= ./logdir/train/2016-10-27T10-38-59/model.ckpt-119452
+ Done.
+step 119453 - loss = 0.951, (22.977 sec/step)
+
+step 119792 - loss = 0.682, (24.751 sec/step)
+
+---
+loss_value= 0.682493 
+---
+
+Storing checkpoint to ./logdir/train/2016-10-27T10-38-59 ...checkpoint_path= ./logdir/train/2016-10-27T10-38-59/model.ckpt-119792
+ Done.
+step 119793 - loss = 0.730, (24.808 sec/step)
+step 119794 - loss = 0.826, (22.661 sec/step)
+
+
+step 127889 - loss = 2.146, (21.177 sec/step)
+step 127890 - loss = 0.691, (21.271 sec/step)
+
+---
+loss_value= 0.69062 
+---
+
+Storing checkpoint to ./logdir/train/2016-10-27T10-38-59 ...checkpoint_path= ./logdir/train/2016-10-27T10-38-59/model.ckpt-127890
+ Done.
+step 127891 - loss = 0.489, (20.880 sec/step)
+
+---
+loss_value= 0.489187 
+---
+
+Storing checkpoint to ./logdir/train/2016-10-27T10-38-59 ...checkpoint_path= ./logdir/train/2016-10-27T10-38-59/model.ckpt-127891
+ Done.
+step 127892 - loss = 0.509, (20.910 sec/step)
+
+---
+loss_value= 0.509363 
+---
+
+Storing checkpoint to ./logdir/train/2016-10-27T10-38-59 ...checkpoint_path= ./logdir/train/2016-10-27T10-38-59/model.ckpt-127892
+ Done.
+step 127893 - loss = 0.516, (20.937 sec/step)
+
+---
+loss_value= 0.5163 
+---
+
+Storing checkpoint to ./logdir/train/2016-10-27T10-38-59 ...checkpoint_path= ./logdir/train/2016-10-27T10-38-59/model.ckpt-127893
+ Done.
+step 127894 - loss = 0.547, (21.032 sec/step)
+
+---
+loss_value= 0.547229 
+---
+
+Storing checkpoint to ./logdir/train/2016-10-27T10-38-59 ...checkpoint_path= ./logdir/train/2016-10-27T10-38-59/model.ckpt-127894
+ Done.
+step 127895 - loss = 3.357, (20.864 sec/step)
+
+
+
+step 136478 - loss = 0.754, (31.625 sec/step)
+step 136479 - loss = 0.538, (27.087 sec/step)
+
+---
+loss_value= 0.537605 
+---
+
+Storing checkpoint to ./logdir/train/2016-10-27T10-38-59 ...checkpoint_path= ./logdir/train/2016-10-27T10-38-59/model.ckpt-136479
+ Done.
+step 136480 - loss = 0.617, (31.770 sec/step)
+
+---
+loss_value= 0.6168 
+---
+
+Storing checkpoint to ./logdir/train/2016-10-27T10-38-59 ...checkpoint_path= ./logdir/train/2016-10-27T10-38-59/model.ckpt-136480
+ Done.
+step 136481 - loss = 0.860, (29.270 sec/step)
+step 136482 - loss = 0.872, (29.990 sec/step)
+step 136483 - loss = 1.659, (29.181 sec/step)
+
+"""
+
+
+
+
+
+loss_str=string.split("loss_value= ") 
+loss_str.pop(0)
+
+#only want first one after the loss declaration otherwise the 500 default saves fuck things up
+dirs=[]
+loss=[]
+
+for s in loss_str:
+	dl =s.split("...checkpoint_path= ")
+	dirs.append(dl[1].split(" ")[0].strip("\n"))
+
+
+	l =s.split(" ")
+	loss.append(l[0].strip(" "))
+	#print ('L[0]',l[0])
+
+
+print ("LENGTH:", len(dirs),len(loss))
+print ("cut and paste below text into RunModels.sh and bash it to generate text\n\n")
+
+
+print("declare -a LOSS=("+" ".join(loss),")\n\n")
+print("for i in "+" ".join(dirs))
